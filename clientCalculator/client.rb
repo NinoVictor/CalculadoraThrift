59f131e8370a2d864866df84b4fc65d9e1885f01
root <root@DESKTOP-VICTOR.localdomain>
2020-05-21 14:30:44 -0500

    client in ruby

---
+++ clientCalculator/client.rb
@@ -0,0 +1,52 @@
+$:.push('gen-rb')
+
+require 'thrift'
+require 'calculator_service'
+require 'optparse'
+require 'ostruct'
+
+# transporte.open()
+
+# result = client.Division(6, 0)
+# puts "Sub de 6 + 4: " + result.result.to_s
+
+# transporte.close()
+def process_argv(option, num1, num2)
+    port = 9090
+
+    transporte = Thrift::Socket.new('localhost', port)
+    transporte = Thrift::BufferedTransport.new(transporte)
+    protocolo = Thrift::BinaryProtocol.new(transporte)
+
+    client = CalculatorService::Client.new(protocolo)
+    transporte.open()
+    case option
+        when "-h"
+        puts "This is the help menu."
+        puts "  -v Enable verbose mode"
+        puts "  -c Enable syntax highlighting"
+        exit
+        when "--sum"
+            result = client.Sum(num1.to_f, num2.to_f)
+            puts "Result: " + result.result.to_s
+            
+        when "--substract"
+            result = client.Subtraction(num1.to_f, num2.to_f)
+            puts "Result: " + result.result.to_s
+        when "--division"
+            result = client.Division(num1.to_f, num2.to_f)
+            puts "Result: " + result.result.to_s
+        when "--multiply"
+            result = client.Multiplication(num1.to_f, num2.to_f)
+            puts "Result: " + result.result.to_s
+    end
+    transporte.close()
+end
+
+@options = {}
+
+
+process_argv(ARGV[0],ARGV[1], ARGV[2])
+  
+
+# p options
